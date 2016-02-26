7cbf75111dc9a4951ab4f2a9dee6b5de2b744d9d
Mael Thomas <mael.thomas@data.gouv.fr>
2016-02-26 18:17:25 +0100

    Rewrite requesting
    Keep in Openfisca.js only what's not specific to the widget

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 var UI = require('./ui.js'),
 	OpenFisca = require('./openfisca.js'),
-	debounce = require('../lib/debounce.js')
+	debounce = require('../lib/debounce.js'),
+	buffer
+
+function getLastResults() { return buffer }
+
+function addBooleanParameters(input) {
+
+	var boolean_parameters = {
+		employee: [ 'stagiaire', 'apprenti' ],
+	}
+
+	for (var provider in boolean_parameters) {
+		var key = document.querySelector('[data-provides="' + provider + '"]').value
+
+		if (boolean_parameters[provider].indexOf(key) > -1)
+			input[key] = true
+	}
+
+	return input
+}
 
 
 /** Handle events from the given form to update data.
@@ -8,7 +27,7 @@ var UI = require('./ui.js'),
 function bindToForm(form) {
 	form.addEventListener('change', UI.reflectParameterChange)
 
-	var handleBasicFormChanges = debounce(OpenFisca.update.bind(form), 300)
+	var handleBasicFormChanges = debounce(openFiscaRequestBuilder(form), 300)
 
 	handleBasicFormChanges()
 
@@ -23,6 +42,62 @@ function bindToForm(form) {
 
 	form.addEventListener('change', handleFormChanges)
 	form.addEventListener('keyup', handleFormChanges)
+}
+
+function openFiscaRequestBuilder(form) {
+	return function() {
+		var input = collectInput(form)
+
+		input = addBooleanParameters(input)
+
+		// Additional parameters
+		var today = new Date()
+		input['contrat_de_travail_debut'] = today.getFullYear() + '-' + (today.getMonth() + 1)
+
+		// Base url containing the list of desired output variables
+		var baseUrl = form.action
+
+		OpenFisca.request(input, baseUrl, handleAPIResponse)
+	}
+}
+
+function handleAPIResponse(error, values, response) {
+	if (error) {
+		if (response && response.error)
+			return UI.showError(response.error)
+
+		UI.showError({ message: error })
+		throw error
+	}
+
+	buffer = values
+	UI.display(values)
+}
+
+// Collect form fields that will be the API input
+function collectInput(form) {
+	var result = {},
+		elements = form.elements
+
+	// switch to reduce
+	Array.prototype.forEach.call(elements, function(element) {
+		if (! element.name)
+			return
+
+		var value = element.value
+
+		if (element.type == 'number')
+			value = Number(element.value.replace(',', '.'))	// IE doesn't support locale number formats
+
+		/* We are simulating a recruitment,
+		hence requesting salaries with the new size of the entreprise */
+		if (element.name == 'effectif_entreprise')
+			value ++
+
+		result[element.name] = value
+	})
+
+	return result
 }
 
 /*
@@ -69,4 +144,7 @@ var jsNodes = document.querySelectorAll('.SGMAPembauche .js-only')
 for (var i = 0; i < jsNodes.length; i++)
 	jsNodes[i].className = jsNodes[i].className.replace('js-only', '')
 
-module.exports.OpenFisca = OpenFisca
+module.exports = {
+	request: OpenFisca.request,
+	getLastResults: getLastResults,
+}
--- js/openfisca.js
+++ js/openfisca.js
@@ -1,56 +1,5 @@
-var UI = require('./ui')
-
-
-var buffer
-
 module.exports = {
-	buildURL: buildOpenFiscaQueryURL,
-	get: get,
-	update: update,
-	getLastResults: function() { return buffer },
-}
-
-
-function serialize(form) {
-	var result = [],
-		elements = form.elements
-
-	Array.prototype.forEach.call(elements, function(element) {
-		if (! element.name)
-			return
-
-		var value = element.value
-
-		if (element.type == 'number')
-			value = Number(element.value.replace(',', '.'))	// IE doesn't support locale number formats
-
-		/* We are simulating a recruitment,
-		hence requesting salaries with the new size of the entreprise */
-		if (element.name == 'effectif_entreprise')
-			value ++
-
-		result.push(encodeURI(element.name + '=' + value))
-	})
-
-	return result.join('&')
-}
-
-
-var BOOLEAN_PARAMETERS = {
-	employee: [ 'stagiaire', 'apprenti' ],
-}
-
-function getAdditionalParameters() {
-	var result = {}
-
-	for (var provider in BOOLEAN_PARAMETERS) {
-		var key = document.querySelector('[data-provides="' + provider + '"]').value
-
-		if (BOOLEAN_PARAMETERS[provider].indexOf(key) > -1)
-			result[key] = true
-	}
-
-	return result
+	request: request,
 }
 
 /** Serializes a shallow object into a series of query string parameters.
@@ -71,35 +20,14 @@ function serializeObject(source) {
 	return result.join('&')
 }
 
-/** Creates an OpenFisca URL to the /formula endpoint, based on the current main form state and the given additional parameters.
+/** Helper to call the OpenFisca Web API's /formula endpoint
 *
-*@param		{Object}	[additionalParameters]	An object whose properties will be appended to the URL as query-string parameters.
-*@returns	{String}	The URL for the OpenFisca query.
-*/
-function buildOpenFiscaQueryURL(additionalParameters) {
-	var form = document.querySelector('#input form'),
-		queryStringBlocks = [
-			serialize(form),
-			serializeObject(getAdditionalParameters()),
-			serializeObject(additionalParameters),
-		].filter(function(element) {
-			return element !== ''}
-		)
-
-	return form.action + '?' + queryStringBlocks.join('&')
-}
-
-/** Computes values based on the current main form state and the given additional parameters.
-*
-*@param	{Object}	[additionalParameters]	An object whose properties will be appended to the URL as query-string parameters.
+*@param	{Object}	[input]	An object whose properties will be appended to the URL as query-string parameters representing OpenFisca input variable ids and their values.
+*@param	{String}	[baseUrl]	Url pointing to an instance of the OpenFisca Web API and containing the desired output variables
 *@param	{Function<[XMLHttpRequest|SyntaxError], Object, Object>}	callback	A callback that will be called with three parameters: an optional error if something went wrong, the OpenFisca-computed values, and the full OpenFisca response if you want everything it sends back.
 */
-function get(additionalParameters, callback) {
-	if (! callback) {
-		callback = additionalParameters
-		additionalParameters = null
-	}
 
+function request(input, baseUrl, callback) {
 	var request = new XMLHttpRequest()
 
 	request.onload = function openFiscaOnloadHandler() {
@@ -114,27 +42,8 @@ function get(additionalParameters, callback) {
 
 	request.onerror = callback.bind(null, request)
 
-	request.open('GET', buildOpenFiscaQueryURL(additionalParameters))
+	var url = baseUrl + '?' + serializeObject(input)
+
+	request.open('GET', url)
 	request.send()
-}
-
-/** Updates the displayed values.
-*/
-function update() {
-	var today = new Date()
-
-	get({
-		contrat_de_travail_debut: today.getFullYear() + '-' + (today.getMonth() + 1),
-	}, function(error, values, response) {
-		if (error) {
-			if (response && response.error)
-				return UI.showError(response.error)
-
-			UI.showError({ message: error })
-			throw error
-		}
-
-		buffer = values
-		UI.display(values)
-	})
 }
